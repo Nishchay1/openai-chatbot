c642043817b2d8da2725460cd30318a8afbe3f52
Nishchay Arjyal <n.arjyal4212@gmail.com>
2024-12-24 20:14:43 -0500

    Add listview instead of carousel.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,7 +8,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var screenList = ['/buddha_screen', '/aristotle_screen', '/confucius_screen', '/lincoln_screen'];
+  var screenList = [
+    '/buddha_screen',
+    '/aristotle_screen',
+    '/confucius_screen',
+    '/lincoln_screen',
+    '/caesar_screen'
+  ];
+  var imageList = [
+    'assets/images/buddha.png',
+    'assets/images/aristotle.png',
+    'assets/images/confucius.png',
+    'assets/images/lincoln.png',
+    'assets/images/caesar.png'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,23 +33,19 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: Container(
           color: Colors.black,
-          child: Center(
-                  child: CarouselView(
-                    backgroundColor: Colors.black,
-                    onTap: (index) {
+          child: ListView.builder(
+              itemCount: imageList.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                    onTap: () {
                       Navigator.pushNamed(context, screenList[index]);
                     },
-                    itemSnapping: true,
-                    itemExtent: MediaQuery.sizeOf(context).width - 40,
-                    shrinkExtent: 250,
-                    children: [
-                      Image.asset('assets/images/buddha.png',),
-                      Image.asset('assets/images/aristotle.png',),
-                      Image.asset('assets/images/confucius.png',),
-                      Image.asset('assets/images/lincoln.png'),
-                      Image.asset('assets/images/caesar.png'),
-                    ],
-                  )),
+                    child: Card(
+                      color: Colors.black,
+                      margin: EdgeInsets.all(8.0),
+                      child: Image.asset(imageList[index]),
+                    ));
+              }),
         ));
   }
 }
