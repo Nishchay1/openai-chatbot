92745c8857197c34abbb281e4114181e51bece35
Nishchay Arjyal <n.arjyal4212@gmail.com>
2025-01-02 15:10:19 -0500

    Add better card design with shadow effect.

--- lib/shared/figure_select_card.dart
+++ lib/shared/figure_select_card.dart
@@ -16,18 +16,38 @@ class _FigureSelectCardState extends State<FigureSelectCard> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.black,
-          shape: BoxShape.circle,
-          border: Border.all(
-            color: const Color.fromARGB(255, 0, 0, 0),
-            width: 10.0,
-          )),
-      width: 300,
-      height: 300,
-      margin: EdgeInsets.all(10.0),
-      child: Hero(
-          tag: widget.heroTag,
-          child: ClipOval(child: Image.asset(widget.imagePath))),
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(4.0, 4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0
+          ),
+          BoxShadow(
+            color: Colors.white,
+            offset: Offset(-4.0, -4.0),
+            blurRadius: 15.0,
+            spreadRadius: 1.0
+          ),
+        ]
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.black,
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: const Color.fromARGB(255, 0, 0, 0),
+              width: 10.0,
+            )),
+        width: 300,
+        height: 300,
+        margin: EdgeInsets.all(10.0),
+        child: Hero(
+            tag: widget.heroTag,
+            child: ClipOval(child: Image.asset(widget.imagePath))),
+      ),
     );
   }
 }
